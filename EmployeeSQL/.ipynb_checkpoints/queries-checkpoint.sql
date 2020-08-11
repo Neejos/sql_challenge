3d2a4c9042a5165f6b6bdb4df0adba49ff8bf05f
Neejos <neejos@yahoo.co.in>
2020-08-10 23:41:07 -0400

    Final commit

---
+++ EmployeeSQL/.ipynb_checkpoints/queries-checkpoint.sql
@@ -0,0 +1,75 @@
+#1. List the following details of each employee: employee number, last name, first name, sex, and salary.
+select employees.emp_no,
+    employees.first_name,
+    employees.last_name,
+    employees.sex,
+    salaries.salary
+from employees
+inner join salaries
+on employees.emp_no=salaries.emp_no;
+
+#2. List first name, last name, and hire date for employees who were hired in 1986.
+select first_name,last_name,hire_date
+from employees
+where hire_date between '1986-01-01' and '1986-01-31'
+order by hire_date;
+
+#3. List the manager of each department with the following information: department number, department name, 
+#the manager's employee number, last name, first name.
+
+select dept_manager.dept_no,departments.dept_name,dept_manager.emp_no,
+    employees.last_name,employees.first_name
+from dept_manager
+inner join departments
+on dept_manager.dept_no=departments.dept_no
+inner join employees
+on dept_manager.emp_no=employees.emp_no;
+
+#4.List the department of each employee with the following information: employee number, last name, first name, and department name.
+select dept_emp.emp_no,employees.last_name,employees.first_name,
+    departments.dept_name
+from dept_emp
+inner join departments 
+on dept_emp.dept_no=departments.dept_no
+inner join employees
+on dept_emp.emp_no=employees.emp_no;
+
+#5.List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+select first_name,last_name,sex
+from employees
+where first_name='Hercules'
+and last_name like 'B%';
+
+#6.List all employees in the Sales department, including their employee number, last name, first name, and department name.
+create view emp_sales as
+select employees.emp_no,employees.first_name,
+    employees.last_name,departments.dept_name
+from employees
+join dept_emp
+on employees.emp_no=dept_emp.emp_no  
+join departments
+on departments.dept_no=dept_emp.dept_no
+where departments.dept_no='d007'
+order by employees.emp_no;
+
+#table with all the required columns and the corresponding data
+select * from emp_sales
+
+#7.List all employees in the Sales and Development departments, including their employee number, last name,
+#first name, and department name.
+select dept_emp.emp_no,employees.last_name,employees.first_name,
+    departments.dept_name
+from dept_emp
+join employees
+on employees.emp_no = dept_emp.emp_no
+join departments
+on departments.dept_no=dept_emp.dept_no
+where departments.dept_name='Sales'
+or departments.dept_name ='Development'
+
+#8.In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+select count(*),last_name
+from employees
+group by last_name
+order by count(*) desc;
+
---
+++ EmployeeSQL/schema.sql
@@ -0,0 +1,45 @@
+
+  
+create table departments(
+    dept_no varchar(20),
+    dept_name varchar(30),
+    primary key (dept_no));
+    
+create table employees(
+    emp_no int,
+    emp_title_id varchar(30),
+    birth_date date,
+    first_name varchar(30),
+    last_name varchar(30),
+    sex varchar(4),
+    hire_date date,
+    primary key (emp_no),
+    foreign key (emp_title_id) references titles(title_id));
+
+    
+create table dept_emp(
+    emp_no int,
+    dept_no varchar(20),
+    CONSTRAINT PK_dept_emp PRIMARY KEY (emp_no,dept_no),
+    foreign key (emp_no) references employees(emp_no),
+    foreign key(dept_no) references departments(dept_no));
+
+    
+
+create table dept_manager(
+    dept_no varchar(30),
+    emp_no int,
+    CONSTRAINT PK_dept_manager PRIMARY KEY (dept_no,emp_no),    
+    foreign key(dept_no) references departments(dept_no),
+    foreign key(emp_no) references employees(emp_no));
+    
+create table salaries(
+    emp_no int,
+    salary int,
+    primary key(emp_no);
+    
+create table titles(
+    title_id varchar(10),
+    title varchar(30),
+    primary key (title_id));
+    
