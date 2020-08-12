be4656b5b11d02b06f0f18d29e26f2bae4302ee0
Neejos <neejos@yahoo.co.in>
2020-08-12 16:04:48 -0400

    Deleted cofig file

---
+++ EmployeeSQL/.ipynb_checkpoints/bonus1-checkpoint.sql
@@ -0,0 +1,13 @@
+--create a view of avg Salary by title
+
+create view title_emp As
+select Round(Avg(s.salary),2) As "Avg_Salary",t.title
+from (employees as e
+join salaries as s
+on e.emp_no=s.emp_no)
+JOIN titles as t
+on  e.emp_title_id = t.title_id
+group by t.title;
+ 
+select * from title_emp
+
